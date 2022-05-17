d65028301717611768c777493701a3caa5a16c4d
flattte <jakubwieczorek01@gmail.com>
2022-05-17 22:10:55 +0200

    remove useless files

--- strict.py
+++ strict.py
@@ -4,16 +4,14 @@ from parser.parserutils import open_files
 from ssh.regular import SSHCisco
 
 
-
-
 if __name__ == "__main__":
 
     verbose = True
     config, target = open_files("test/cfg.txt", "test/strict.txt")
 
-
     conn = SSHCisco()
-    conn.connect("192.168.43.32", 22, username="cisco", password="cisco", timeout=5)
+    conn.connect("192.168.43.32", 22, username="cisco",
+                 password="cisco", timeout=5)
     if conn.isConnected():
         config = conn.readConfig()
 
