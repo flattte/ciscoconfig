5d911382d79e6543c9aa7fae20f114881cd2b5cf
flattte <jakubwieczorek01@gmail.com>
2022-09-18 20:34:35 +0200

    kinda works

--- nb_parser/parser.py
+++ nb_parser/parser.py
@@ -1,4 +1,9 @@
 
+from distutils.debug import DEBUG
+
+
+DEBUG = True
+
 def open_file(path):
     assert type(path) is str, "path must be a string preferably valid xd"
     try:
@@ -8,21 +13,19 @@ def open_file(path):
     return f
 
 
-class token(object):
-    def __init___(self, content):
-        self.content = content
-        self.token_is_valid = True
-        if not content:
-            self.token_is_valid = False    
-            return None
-        self.title = content[0]
-        if not content[1:]: return
-        self.body = content[1:]
-
-
 class mode:
     active = 0
     passive = 1
+
+class token_wrapper(object):
+    def __init__(self, content):
+        self.content = content
+        self.title = None
+        self.body = None 
+        if content:
+            self.title = content[0]
+        if content[1:]:
+            self.body = content[1:]
 
 class parser(object):
     def __init__(self, config, target,
@@ -38,40 +41,49 @@ class parser(object):
         print("configuration")
         print(configuration)
         state = mode.passive 
-        bad_lines = ['Current configuration :', 'version']
+        bad_lines = ['show run', 'Building', 'Current configuration :', 'version']
         ret_tokens = []
-        token = []
+        raw_text_token = []
         for line in configuration:
             if any([bad_line in line for bad_line in bad_lines]):
                 continue
-            if '!' not in line:
-                if state == mode.passive:
-                    state = mode.active
-                token.append(line)
+            if '!' not in line and line:
+                if state == mode.passive: state = mode.active
+                if line[0] == ' ':
+                    raw_text_token.append(line)
+                else: 
+                    ret_tokens.append(token_wrapper(raw_text_token))
+                    raw_text_token = [line]
             else:
+                if state == mode.active:
+                    if raw_text_token: ret_tokens.append(token_wrapper(raw_text_token))
+                    raw_text_token = []
+                    state = mode.passive
                 if state == mode.passive:
                     continue
-                if state == mode.active:
-                    ret_tokens.append(token)
-                    token = []
-                    state = mode.passive 
-    
+        return ret_tokens 
+
+    def print_token(self,token):
+        print("Token title")
+        print("    ", token.title,)
+        if token.body:
+            print("Token body")
+            print("    ", token.body)
+        print("\n")
+
     def parse(self):
-        self.target_tokens = self.tokenize(self.target)
-        self.config_tokens = self.tokenize(self.config)
-        print(self.target_tokens)
-        print(self.config_tokens)
+        self.target_tokens = [token for token in self.tokenize(self.target) if token.title]
+        self.config_tokens = [token for token in self.tokenize(self.config) if token.title]
+        if DEBUG:
+            print([self.print_token(token) for token in self.target_tokens])
+            print([self.print_token(token) for token in self.config_tokens])
                     
-                
-                
-            
-        
 
 if __name__ == "__main__":
     c = open_file("../test/cfg1.txt")
     t = open_file("../test/cfg2.txt")
-    config = [line.strip() for line in c.readlines()]
-    target = [line.strip() for line in t.readlines()]
+    config = [line.strip('\n') for line in c.readlines()]
+    target = [line.strip('\n') for line in t.readlines()]
     p = parser(config, target)
     p.parse()
 
